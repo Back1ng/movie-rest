42ecc7a3ab6d52921e9aa97791df41b0625358da
anatoly <anatolyu128@gmail.com>
2022-02-18 13:21:31 +0500

    form request decode & errors in json for api

---
+++ app/Http/Requests/Api/FormRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest as LaravelFormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+abstract class FormRequest extends LaravelFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    abstract public function authorize();
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+
+    protected function prepareForValidation()
+    {
+        foreach ($this->all() as $key => $value) {
+            if (null !== json_decode($value)) {
+                $this->merge([$key => json_decode($value)]);
+            }
+        }
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json(['errors' => $validator->errors()], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
